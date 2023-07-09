1f2be3bdd396e7a07fb5b4531b8153250310a0fe
Lucas Garcia <58547290+LcsGa@users.noreply.github.com>
2023-07-09 15:43:46 +0200

    refactor: format file

--- v13-.ts
+++ v13-.ts
@@ -6,17 +6,17 @@ import { BehaviorSubject, map } from "rxjs";
     selector: "my-component",
     template: `
         <ng-container *ngIf="myDoubledValue$ | async as doubled">
-        <ng-container *ngIf="doubled === 10; else notMagic">
-            <h2>Congrats!</h2>
-
-            <p>That's the magic number!</p>
-        </ng-container>
-
-        <ng-template #notMagic>
-            <p *ngIf="doubled > 10">Too high...</p>
-
-            <p *ngIf="doubled < 10">Too low...</p>
-        </ng-template>
+            <ng-container *ngIf="doubled === 10; else notMagic">
+                <h2>Congrats!</h2>
+    
+                <p>That's the magic number!</p>
+            </ng-container>
+    
+            <ng-template #notMagic>
+                <p *ngIf="doubled > 10">Too high...</p>
+    
+                <p *ngIf="doubled < 10">Too low...</p>
+            </ng-template>
         </ng-container>
     `,
 })
